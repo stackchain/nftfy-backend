c16f1fc234fb3e0ca0d3a52632171f2311d0a99a
Fabricio Miranda <fabriciomirandabr@gmail.com>
2021-01-13 09:26:54 -0300

    Added Firebase

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,8 @@
 import express from 'express'
+import * as functions from 'firebase-functions'
 import router from './routes'
 
 const app = express()
-const port = 3000
-
 app.use('/', router)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-})
+exports.api = functions.https.onRequest(app)
